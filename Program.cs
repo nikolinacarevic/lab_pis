e2dfe88f626ea287af7a59007276f17af40519ea
nikolinacarevic <ncarev00@fesb.hr>
2023-03-15 17:22:49 +0100

    dodavanje datoteka 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab_pis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
